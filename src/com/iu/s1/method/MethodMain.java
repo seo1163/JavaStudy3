9676c28d6969a3b9db0d035476377f6bed2aa2b3
seo1163 <seo1163@naver.com>
2022-01-04 15:34:30 +0900

    Make Java

--- src/com/iu/s1/method/MethodMain.java
+++ src/com/iu/s1/method/MethodMain.java
@@ -1,10 +1,29 @@
 package com.iu.s1.method;
 
+import java.util.Scanner;
+
 public class MethodMain {
 
 	public static void main(String[] args) {
 		System.out.println("MainMethod Start");
+		Scanner sc = new Scanner(System.in);
 		
+		//객체 생성 코드
+		//new 클래스명();
+		ReturnStudy rs = new ReturnStudy();
+		
+		//참조변수명.멤버들
+		rs.test1();
+		
+		//rs.test2();
+		
+		int count = rs.test2();
+		System.out.println("MAin : " + count);
+		
+		String name = rs.test3();
+		System.out.println(name);
+		
+		Math.random();
 		
 		System.out.println("MainMethod Finish");
 
